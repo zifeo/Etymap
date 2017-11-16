e24991e29c56a5226597aad0511d0e486c4520a7
Arnaud <arnaud.delamare@epfl.ch>
2017-11-16 16:42:13 +0100

    Add basic zoomable map

--- src/app.js
+++ src/app.js
@@ -4,26 +4,54 @@ import 'whatwg-fetch';
 import 'babel-polyfill';
 import $ from 'jquery';
 import * as d3 from 'd3';
-import Datamap from 'datamaps';
+
+
+const geojson = require('./world.geo');
 
 const onReload = module.onReload || (() => {});
 onReload(() => {
   // eslint-disable-next-line
   console.info('hot reloading');
-  // d3.select('#map').remove();
+  d3.select('#map').remove();
 });
 
-$('#content')
-  .html('hello')
-  .addClass('jq');
+let width = $(window).width();
+let height = $(window).height();
 
-d3
-  .select('body')
-  .append('div')
-  .text('world!')
-  .attr('id', 'map');
+const svg = d3.select("body")
+  .append("svg")
+  .attr("style", "background-color:#b7d2ff;")
+  .call(d3.zoom()
+    .scaleExtent([1 / 2, 8])
+    .on("zoom", function() {g.attr("transform", d3.event.transform); }));
 
-const dm = new Datamap({
-  element: document.getElementById('map'),
-});
-dm.legend();
+const g = svg.append("g");
+
+const projection = d3.geoNaturalEarth1()
+    .scale(100);
+
+const geoPath = d3.geoPath().projection(projection);
+
+g.selectAll("path")
+  .data(geojson.map.features)
+  .enter()
+    .append("path")
+    .attr("fill", "#ffe3b7")
+    .attr("stroke-width", "0")
+    .attr("d", geoPath);
+
+rescale();
+
+$(window).resize(rescale);
+
+function rescale() {
+  width = $(window).width();
+  height = $(window).height();
+
+  projection
+    .scale(width/7)
+    .translate([width/2,height/2]);
+
+  g.selectAll("path")
+    .attr("d", geoPath);
+}
