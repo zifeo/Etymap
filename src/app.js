9b0b7b322d100f1b7b98d1378ca903ede1eb1c21
Arnaud <arnaud.delamare@epfl.ch>
2017-11-24 22:05:55 +0100

    Add path animation

--- src/app.js
+++ src/app.js
@@ -11,14 +11,21 @@ const geojson = require('./world.geo');
 let width = $(window).width();
 let height = $(window).height();
 
+let maskID = 0;
+
 const svg = d3.select("body")
   .append("svg")
   .attr("style", "background-color:#b7d2ff;")
   .call(d3.zoom()
     .scaleExtent([1 / 2, 8])
-    .on("zoom", function() {g.attr("transform", d3.event.transform); }));
+    .on("zoom", function() {g.attr("transform", d3.event.transform); defs.attr("transform", d3.event.transform);}));
 
-const g = svg.append("g");
+const defs = svg.append("defs")
+                .append("g")
+                .attr("id", "defs");
+
+const g = svg.append("g")
+              .attr("id", "g");
 
 const projection = d3.geoNaturalEarth1()
   .scale(100);
@@ -38,14 +45,18 @@ g.selectAll("path")
     .attr("class", "mapPath");
 
 let languagesCoo = {};
+const temp = [];
 d3.csv("https://raw.githubusercontent.com/zifeo/Etymap/master/data/languages_coordinates.csv", function(data) { //don't know what path to use to load from the server
   data.forEach(function(d) {
     if (d.longitude && d.latitude && isFinite(String(d.longitude)) && isFinite(String(d.latitude)) && d.isocode) {
       languagesCoo[d.isocode] = d;
+      temp.push(d);
     }
   });
 
-  addLine(['fra', 'deu', 'lat']);
+  addLine(['fra', 'deu', 'lat', 'acy']);
+  addLine(['fra', 'eng', 'gle']);
+  addLine(['fra', 'deu', 'hun', 'rus']);
 });
 
 function addLine(isocodes) {
@@ -55,21 +66,47 @@ function addLine(isocodes) {
     positionsGeo.push([languagesCoo[isocode].longitude, languagesCoo[isocode].latitude]);
   }
 
-  g.selectAll("circle")
-  .data(positionsGeo)
-  .enter()
+  g.selectAll("none") //Circles for each language
+   .data(positionsGeo)
+   .enter()
     .append("circle")
       .attr("cx", function(posGeo) {return projection(posGeo)[0]})
       .attr("cy", function(posGeo) {return projection(posGeo)[1]})
       .attr("r", 2);
 
 
-  g.append("path")
+  let path = g.append("path") //Path that goes through each language
     .data([positionsGeo])
     .attr('d', lineGenerator(positionsGeo.map(posGeo => projection(posGeo))))
     .attr("fill-opacity", 0)
     .attr("stroke", "black")
     .attr("class", "languagePath");
+
+  //We clone the path to create a mask, to animate the original path
+  const cloneID = "clone" + (maskID ++); 
+
+  const cloneMask = defs.append("mask")
+    .attr("id", cloneID);
+
+  let clone = path.node().cloneNode();
+  clone.id = cloneID + "Path";
+  $("#" + cloneID).append(clone);
+
+  var length = Math.ceil(path.node().getTotalLength());
+
+  //We animate the mask
+  d3.select("#" + cloneID + "Path")
+    .data([positionsGeo])
+    .attr("class", "mask")
+    .attr("stroke", "white")
+    .attr("stroke-dasharray", length + ' ' + length)
+    .attr("stroke-dashoffset", length)
+    .transition()
+        .duration(3000)
+        .attr("stroke-dashoffset", 0);
+;
+
+  path.attr("mask", "url(#" + cloneID + ")")
 }
 
 rescale();
@@ -92,5 +129,8 @@ function rescale() {
     .attr("cy", function(posGeo) {return projection(posGeo)[1]});
 
   g.selectAll(".languagePath")
+    .attr('d', function(positionsGeo) { return lineGenerator(positionsGeo.map(posGeo => projection(posGeo))) });
+
+  g.selectAll(".mask")
     .attr('d', function(positionsGeo) { return lineGenerator(positionsGeo.map(posGeo => projection(posGeo))) });
 }
