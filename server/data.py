79e09ea052a2e2bb3136c56ffc84bbdc7078df6e
Teo Stocco <teo@zifeo.com>
2017-12-08 14:41:06 +0100

    add network

--- server/data.py
+++ server/data.py
@@ -2,7 +2,18 @@ import json
 import os
 
 current_dir = os.path.dirname(__file__)
-word_lang_url = os.path.join(current_dir, '../data/word_lang.json')
 
-with open(word_lang_url, 'r') as f:
+
+def open_data(name):
+    return open(os.path.join(current_dir, '../data', name), 'r')
+
+
+with open_data('word_lang.json') as f:
     word_lang_idx = json.load(f)
+
+with open_data('network_from.json') as f:
+    network_from_idx = json.load(f)
+
+with open_data('network_to.json') as f:
+    network_to_idx = json.load(f)
+
--- server/main.py
+++ server/main.py
@@ -1,15 +1,20 @@
 from flask import Flask, send_from_directory, jsonify
+
+from server.data import network_from_idx, network_to_idx
 from .idx import langsFor
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return send_from_directory('../build', 'index.html')
 
+
 @app.route('/process-book')
 def process_book():
     return send_from_directory('../build', 'process-book.html')
+
 
 @app.route('/search/word/<string:word>')
 def word_search(word):
@@ -19,6 +24,7 @@ def word_search(word):
         results=langsFor(word)
     )
     return jsonify(ret)
+
 
 @app.route('/word/<string:word>')
 def word_info(word):
@@ -31,6 +37,17 @@ def word_info(word):
         lang=langsFor(word)
     )
     return jsonify(ret)
+
+
+@app.route('/network/from')
+def network_from():
+    return jsonify(network_from_idx)
+
+
+@app.route('/network/to')
+def network_to():
+    return jsonify(network_to_idx)
+
 
 @app.route('/<path:path>')
 def static_files(path):
